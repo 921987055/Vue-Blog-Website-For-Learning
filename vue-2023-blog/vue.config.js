c0ca4d56ded4ef35cacaf1c6449e3a5c373791fe
92198 <921987055@qq.com>
2023-08-02 17:29:23 +0800

    Update webpack config

--- vue-2023-blog/vue.config.js
+++ vue-2023-blog/vue.config.js
@@ -1,5 +1,6 @@
 const { defineConfig } = require('@vue/cli-service')
 const TerserPlugin = require("terser-webpack-plugin")
+const CompressionWebpackPlugin = require('compression-webpack-plugin')
 
 module.exports = defineConfig({
   transpileDependencies: true,
@@ -15,6 +16,16 @@ module.exports = defineConfig({
     optimization: {
       minimize: true,
       minimizer: [new TerserPlugin()],
-    }
+    },
+    plugins: [
+      new CompressionWebpackPlugin({
+        // algorithm: 'gzip', // 使用 gzip 压缩
+        test: /\.js$|\.html$|\.css$/, // 匹配文件名
+        // filename: '[path].gz[query]', // 压缩后的文件名(保持原文件名, 后缀加 .gz)
+        minRatio: 1, // 压缩率小于1才会压缩
+        threshold: 10240, // 对超过10k的数据压缩
+        deleteOriginalAssets: true // 是否删除未压缩的源文件, 谨慎设置, 若果希望提供非gzip的资源, 可不设置或者设置为 false
+      }),
+    ],
   }
 })
